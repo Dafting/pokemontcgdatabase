fac5075e83ad5fc7d15136139d7b50360d906cee
Joaquín de la Iglesia <joaquindlaiglesia@gmail.com>
2021-11-28 13:51:41 -0300

    Borrada función duplicada
    La misma función se encuentra en pokecards.controller.php así que ya no es necesaria en este fichero.

--- controllers/cards.controller.php
+++ controllers/cards.controller.php
@@ -37,28 +37,4 @@ class CardsController {
             //$this->view->showAddEnergyCard();
         }
     }
-    
-    function addPokeCard() {
-        $type = $_REQUEST['type'];
-        $hp = $_REQUEST['hp'];
-        $stage = $_REQUEST['stage'];
-        $attackName1 = $_REQUEST['attackName1'];
-        $attackDesc1 = $_REQUEST['attackDesc1'];
-        $attackDamage1 = $_REQUEST['attackDamage1'];
-        $attackEnergies1 = $_REQUEST['attackEnergies1'];
-        $attackName2 = $_REQUEST['attackName2'];
-        $attackDesc2 = $_REQUEST['attackDesc2'];
-        $attackDamage2 = $_REQUEST['attackDamage2'];
-        $attackEnergies2 = $_REQUEST['attackEnergies2'];
-        $hasPokePower = $_REQUEST['hasPokePower'];
-        $pokePowerName = $_REQUEST['pokePowerName'];
-        $pokePowerDesc = $_REQUEST['pokePowerDesc'];
-        $weakness = $_REQUEST['weakness'];
-        $resistance = $_REQUEST['resistance'];
-        $retreatCost = $_REQUEST['retreatCost'];
-        $pokedexInfo = $_REQUEST['pokedexInfo'];
-        //$image = $_REQUEST['image'];
-    
-        $this->model->insertCard($type, $hp, $stage, $attackName1, $attackDesc1, $attackDamage1, $attackEnergies1, $attackName2, $attackDesc2, $attackDamage2, $attackEnergies2, $hasPokePower, $pokePowerName, $pokePowerDesc, $weakness, $resistance, $retreatCost, $pokedexInfo);
-    }
 }
